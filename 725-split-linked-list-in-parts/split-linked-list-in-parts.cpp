15bdbd6aeac154e7c4e5e870df3af4c0e7dc7f18
Jayeshgarud <jayeshgarud530@gmail.com>
2026-08-30 23:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (48.20%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-
-       
-        int len = 0;
+int len = 0;
         ListNode* temp = head;
 
         while (temp) {
             len++;
             temp = temp->next;
         }
+int size = len / k;
+int extra = len % k;
 
-       
-        int size = len / k;
-
-        
-        int extra = len % k;
-
-        vector<ListNode*> ans;
-
-        for (int i = 0; i < k; i++) {
-
-           
-            ListNode* partHead = head;
-
-           
-            int currSize = size + (i < extra);
-
-            if (currSize == 0) {
+vector<ListNode*> ans;
+for (int i = 0; i < k; i++) {
+ListNode* partHead = head;
+int currSize = size + (i < extra);
+if (currSize == 0) {
                 ans.push_back(nullptr);
                 continue;
             }
-
-           
-            for (int j = 1; j < currSize; j++) {
+for (int j = 1; j < currSize; j++) {
                 head = head->next;
             }
-
-           
-            ListNode* next = head->next;
-
-           
+           ListNode* next = head->next;
             head->next = nullptr;
-
-           
-            ans.push_back(partHead);
-
-           
-            head = next;
+                ans.push_back(partHead);
+                    head = next;
         }
-
-        return ans;
+return ans;
     }
 };
